a41640568c2cee3fc7e9d46de920b1fbc4fbd43a
Tim Standen <timmstanden@gmail.com>
2023-06-05 20:31:55 -0700

    switch to date picker and email generator

--- src/model-helpers/User.js
+++ src/model-helpers/User.js
@@ -8,7 +8,8 @@ import {
   getDatetime,
   getStringNoLocale,
   getUrl,
-  getThing
+  getThing,
+  getDate
 } from '@inrupt/solid-client';
 import { RDF_PREDICATES } from '../constants';
 
@@ -119,7 +120,7 @@ const loadUserActivity = async (podUrl, session) => {
  * @returns {Promise<object>} Promise - Updates last active time of user to lastActive.ttl
  */
 export const createUser = (userSubmission, session) => {
-  const { familyName, username, givenName, webId, podUrl } = userSubmission;
+  const { familyName, username, givenName, webId, podUrl, email, dateOfBirth } = userSubmission;
 
   loadUserActivity(podUrl, session);
 
@@ -129,7 +130,9 @@ export const createUser = (userSubmission, session) => {
     givenName,
     webId,
     dateModified: null,
-    podUrl
+    podUrl,
+    email,
+    dateOfBirth
   };
 };
 
@@ -147,10 +150,22 @@ export const parseUserFromThing = async (userThing, session) => {
   const givenName = getStringNoLocale(userThing, RDF_PREDICATES.givenName);
   const familyName = getStringNoLocale(userThing, RDF_PREDICATES.familyName);
   const username = getStringNoLocale(userThing, RDF_PREDICATES.alternateName);
+  const email = getStringNoLocale(userThing, RDF_PREDICATES.email);
+  const dateOfBirth = getDate(userThing, RDF_PREDICATES.dateOfBirth);
   const webId = getUrl(userThing, RDF_PREDICATES.identifier);
   const podUrl = getUrl(userThing, RDF_PREDICATES.URL);
   const dateModified = await loadUserActivity(podUrl, session);
-  return { person, username, givenName, familyName, webId, podUrl, dateModified };
+  return {
+    person,
+    username,
+    givenName,
+    familyName,
+    webId,
+    podUrl,
+    dateModified,
+    dateOfBirth,
+    email
+  };
 };
 
 /**
@@ -165,14 +180,26 @@ export const parseUserFromThing = async (userThing, session) => {
  * @param {string} userObject.familyName - family name
  * @param {string} userObject.webId - web id
  * @param {string} userObject.podUrl - pod url
- * @returns {ThingLocal} - resulting thing to be stored
+ * @param {string} userObject.email - User's Email
+ * @param {string} userObject.dateOfBirth - Date of birth
+ * @returns {import('@inrupt/solid-client').Thing} - resulting thing to be stored
  */
-export const makeUserIntoThing = ({ username, givenName, familyName, webId, podUrl }) =>
+export const makeUserIntoThing = ({
+  username,
+  givenName,
+  familyName,
+  webId,
+  podUrl,
+  email,
+  dateOfBirth
+}) =>
   buildThing(createThing({ name: username }))
     .addStringNoLocale(RDF_PREDICATES.Person, `${givenName} ${familyName}`)
     .addStringNoLocale(RDF_PREDICATES.givenName, givenName)
     .addStringNoLocale(RDF_PREDICATES.familyName, familyName)
     .addStringNoLocale(RDF_PREDICATES.alternateName, username)
+    .addStringNoLocale(RDF_PREDICATES.email, email)
+    .addDate(RDF_PREDICATES.dateOfBirth, dateOfBirth)
     .addUrl(RDF_PREDICATES.identifier, webId)
     .addUrl(RDF_PREDICATES.URL, podUrl)
     .build();
